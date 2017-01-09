7d7c5638aca1ba72b19938a922915b2937226075
Matt Millsaps-Brewer <matt@madebylotus.com>
2017-01-09 15:41:11 -0500

    Adds test suite setup

---
+++ spec/support/shoulda_matchers.rb
@@ -0,0 +1,11 @@
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    # Choose a test framework:
+    with.test_framework :rspec
+
+    # Choose one or more libraries:
+    with.library :active_record
+    with.library :active_model
+    with.library :action_controller
+  end
+end
