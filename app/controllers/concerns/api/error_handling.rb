3ee41affb1a1ef0c88b4d34debb846a010073d55
Matt Millsaps-Brewer <matt@madebylotus.com>
2017-01-10 17:05:49 -0500

    Allow raising exception to render a 422 error in API

--- app/controllers/concerns/api/error_handling.rb
+++ app/controllers/concerns/api/error_handling.rb
@@ -1,10 +1,16 @@
 module API
+  UnprocessableEntityError = Class.new(StandardError)
+
   module ErrorHandling
     extend ActiveSupport::Concern
 
     included do
       rescue_from ActiveRecord::RecordNotFound do |e|
         render json: e, serializer: ExceptionSerializer, status: :not_found
+      end
+
+      rescue_from UnprocessableEntityError do |e|
+        render json: e, serializer: ExceptionSerializer, status: :unprocessable_entity
       end
 
       rescue_from StandardError do |e|
