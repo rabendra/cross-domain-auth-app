5a0b68876e42cb5fc4988fc0bf4fe08703825e3f
Matt Millsaps-Brewer <matt@madebylotus.com>
2017-01-10 16:39:44 -0500

    Handle common Rails exceptions in API

--- app/controllers/concerns/api/error_handling.rb
+++ app/controllers/concerns/api/error_handling.rb
@@ -3,6 +3,10 @@ module API
     extend ActiveSupport::Concern
 
     included do
+      rescue_from ActiveRecord::RecordNotFound do |e|
+        render json: e, serializer: ExceptionSerializer, status: :not_found
+      end
+
       rescue_from StandardError do |e|
         render json: e, serializer: ExceptionSerializer, status: :bad_request
       end
