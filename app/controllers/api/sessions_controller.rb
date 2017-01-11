d9598c5ec2d5f659675490a0e9c7264b37be910c
Matt Millsaps-Brewer <matt@madebylotus.com>
2017-01-10 19:46:16 -0500

    Adds tests for API::SessionsController

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,23 +1,24 @@
-class API::SessionsController < Devise::SessionsController
-  include API::ErrorHandling
-  include API::ForgeryProtection
+module API
+  class SessionsController < Devise::SessionsController
+    include ErrorHandling
+    include ForgeryProtection
 
-  InvalidUsernameOrPassword = Class.new(StandardError)
+    def create
+      user = User.find_by(email: sign_in_params[:email])
 
-  def create
-    user = User.find_by_email(sign_in_params[:email])
-
-    if user && user.valid_password?(sign_in_params[:password])
-      user.user_token = AuthenticationToken.encode(user.token_payload)
-      render json: user, serializer: UserSerializer, status: :ok
-    else
-      raise InvalidUsernameOrPassword, 'Invalid username or passord'
+      if user && user.valid_password?(sign_in_params[:password])
+        user.user_token = AuthenticationToken.encode(user.token_payload)
+        
+        render json: user, serializer: UserSerializer
+      else
+        raise UnprocessableEntityError, 'Invalid username or password'
+      end
     end
-  end
 
-  private
+    private
 
-  def sign_in_params
-    params.permit(:email, :password)
+    def sign_in_params
+      params.permit(:email, :password)
+    end
   end
 end
