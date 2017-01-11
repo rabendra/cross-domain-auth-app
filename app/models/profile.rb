ce8b78448875c764ff92cb71c56b7eab65b99a4e
Matt Millsaps-Brewer <matt@madebylotus.com>
2017-01-10 21:57:47 -0500

    Adds avatar uploader to Profile model

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,5 +2,7 @@ class Profile < ApplicationRecord
   belongs_to :user, required: true, inverse_of: :profile
   has_one :address, as: :addressable, dependent: :destroy, required: true
 
+  mount_uploader :avatar, ProfileUploader
+
   validates :first_name, presence: true
 end
