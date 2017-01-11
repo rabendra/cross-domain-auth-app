a33b6f906102354fe9f84e4dfaa0666d8d5dd635
kgd <kylegraydev@gmail.com>
2017-01-11 09:40:26 -0800

    Adds phone number formatting to just digits

--- app/controllers/api/registrations_controller.rb
+++ app/controllers/api/registrations_controller.rb
@@ -4,23 +4,15 @@ module API
     include ForgeryProtection
 
     def create
-        user = User.create(sign_up_params)
-        if user.valid?
-
-          user.user_token = AuthenticationToken.encode(user.token_payload)
-
-          render json: user, serializer: RegistrationSerializer
-        else
-            raise UnprocessableEntityError, 'Invalid sign up'
-          #TODO get validation errors from devise
-        end
+        user = User.create!(sign_up_params)
+        user.user_token = AuthenticationToken.encode(user.token_payload)
+        render json: user, serializer: RegistrationSerializer
     end
 
   private
 
     def sign_up_params
       params.permit(:email, :password, :phone_number)
-      #TODO are we handling password_confirmation? are we formatting phone number
     end
 
   end
