4e6860e9022fcc84e512d5922c8360f703e373b8
Matt Millsaps-Brewer <matt@madebylotus.com>
2017-01-12 15:51:24 -0500

    Removes UserSerializer in favor of profiles

--- spec/serializers/user_serializer_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe UserSerializer, type: :serializer do
-  let(:resource) { create(:user) }
-
-  it { is_expected.to include(id: resource.id) }
-  it { is_expected.to include(email: resource.email) }
-end
