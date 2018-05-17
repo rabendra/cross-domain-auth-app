273895688dcee21df836c2902aa8b997ca8aa248
rabnendra sharma <rabnendrayuvasoft166@gmail.com>
2018-05-17 18:25:35 +0530

    #Feature new change

--- app/controllers/api/registrations_controller.rb
+++ app/controllers/api/registrations_controller.rb
@@ -32,6 +32,7 @@ module API
 
     def encode_and_return_token_json(user)
       user.user_token = AuthenticationToken.encode(user.token_payload)
+      puts "#{user.inspect}"
       render json: {id: user.id, accessToken: user.user_token, created_at: user.created_at, updated_at: user.updated_at, username: user.username}
     end
 
